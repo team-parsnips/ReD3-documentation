1f8769ad39de1dd372d21d4b1aba81d0dff6ba96
Rob0h <robert.is.oh@gmail.com>
2016-12-26 14:31:20 -0800

    Initial commit of sequence sunburst - Mouseover does not work after leaving boundary - Colors not working - No filtering of nodes

--- src/client/app/sunburst/sunburst.jsx
+++ src/client/app/sunburst/sunburst.jsx
@@ -22,10 +22,7 @@ class SunBurst extends React.Component {
     var y = d3.scaleSqrt()
         .range([0, radius]);
 
-    var svg = d3.select("body").append("svg")
-        .attr("width", width)
-        .attr("height", height)
-      .append("g")
+    var svg = d3.select(".sunburst").append("g")
         .attr("transform", "translate(" + width / 2 + "," + (height / 2 + 10) + ")");
 
     var partition = d3.partition();
