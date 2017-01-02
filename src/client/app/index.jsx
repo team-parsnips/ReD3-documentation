20da6dcf023b679675bad2d49a4a9ace4a1000a5
Rob0h <robert.is.oh@gmail.com>
2017-01-02 14:26:58 -0800

    Implement hierarchical edge bundling (WIP)

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,22 +1,18 @@
 import React from 'react';
 import {render} from 'react-dom';
 
-
 import Voronoi from './voronoi/voronoi.jsx';
 import SunBurst from './sunburst/sunburst.jsx';
 import SequenceSunBurst from './sunburst/sequence.jsx';
 import DndTree from './dnd-tree/dndTree.jsx';
 import ZoomableMap from './zoomableMap/zoomableMap.jsx';
+import HierarchialEdgeBundle from './hierarchical/hierarchical.jsx';
 
 class App extends React.Component {
   render () {
     return (    
       <div>
-        <Voronoi />
-        <SunBurst />
-        <SequenceSunBurst />
-        <DndTree />
-        <ZoomableMap />
+        <HierarchialEdgeBundle />
       </div>
     );
   }
