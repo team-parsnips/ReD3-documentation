522e9ba0953da1609a2a46f51a6fe7f5b5f7f2fa
Emerson Hum <emersonhum@gmail.com>
2017-01-02 18:16:49 -0800

    Complete grouped/stacked bar chart

--- src/client/app/barchart/stackedgroupedbar.jsx
+++ src/client/app/barchart/stackedgroupedbar.jsx
@@ -6,7 +6,7 @@ const margin = {
   right: 10,
   bottom: 20,
   left: 10
-};
+};  
 
 class StackedGroupedBar extends React.Component {
   constructor(props) {
@@ -16,7 +16,8 @@ class StackedGroupedBar extends React.Component {
       innerWidth: (this.props.width || 960) - margin.left - margin.right,
       height: this.props.height || 500,
       innerHeight: (this.props.height ||500) - margin.top - margin.bottom,
-      dataSet: {}
+      data: this.props.data || null,
+      dataSet: null
 
     }
     this.sampleData = this.sampleData.bind(this);
@@ -25,7 +26,6 @@ class StackedGroupedBar extends React.Component {
   }
 
   sampleData() {
-    console.log(margin.top);
     let m = 58, n = 4, xz, yz, y01z, yMax, y1Max, dataSet;
     xz = d3.range(m);
     yz = d3.range(n).map(()=> this.bumps(m));
@@ -41,38 +41,77 @@ class StackedGroupedBar extends React.Component {
       yMax: yMax,
       y1Max: y1Max
     };
-
     this.setState({ dataSet: dataSet });
   }
 
   componentWillMount() {
-    this.sampleData();
+    if (!this.state.data) {
+      this.sampleData();
+    } else {
+      this.setUpData();
+    }
+  }
+
+  setUpData() {
+    var m, n, xz, yz, y01z, yMax, y1Max, dataSet, transposedData = [], data = this.state.data;
+    m = data['x-ticks'] || 25;
+    xz = data['x-axis'] || d3.range(m);
+    yz = data.data;
+    n = Object.keys(yz).length;
+
+    for (let i = 0; i < m; i++) {
+      let entry = {};
+      for (let key in yz) {
+        entry[key] = yz[key][i]
+      }
+      transposedData.push(entry);
+    }
+
+    y01z = d3.stack().keys(Object.keys(yz)).order(d3.stackOrderNone).offset(d3.stackOffsetNone)(transposedData);
+    yMax = d3.max(Object.keys(yz), y => d3.max(yz[y]));
+    y1Max = d3.max(y01z, y => d3.max(y, d => d[1]));
+    dataSet = {
+        m: m,
+        n: n,
+        xz: xz,
+        yz: yz,
+        y01z: y01z,
+        yMax: yMax,
+        y1Max: y1Max
+      };
+
+    this.setState({ dataSet: dataSet });
   }
 
   componentDidMount() {
-    var svg, g, x, y, color, series, rect;
-    let data = this.state.dataSet;
+    var svg, g, x, y, color, series, rect, timeout, data = this.state.dataSet;
 
-    svg = d3.select('.stackedgrouped');
-    g = svg.append('g').attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
-    x = d3.scaleBand().domain(data.xz).rangeRound([0, this.state.innerWidth]).padding(0.08);
-    y = d3.scaleLinear().domain([0, data.y1Max]).range([this.state.innerHeight, 0]);
-    color = d3.scaleOrdinal().domain(d3.range(data.n)).range(d3.schemeCategory20c);
-    series = g.selectAll('.series').data(data.y01z).enter().append('g').attr('fill', (d, i) => color(i));
-    rect = series.selectAll('rect').data(d => d).enter().append('rect')
-    .attr('x', (d, i) => x(i)).attr('y', this.state.innerHeight).attr('width', x.bandwidth()).attr('height', 0);
+       svg = d3.select('.stackedgrouped');
+         g = svg.append('g')
+              .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+         x = d3.scaleBand().domain(d3.range(data.m)).rangeRound([0, this.state.innerWidth]).padding(0.08);
+         y = d3.scaleLinear().domain([0, data.y1Max]).range([this.state.innerHeight, 0]);
+     color = d3.scaleOrdinal().domain(d3.range(data.n)).range(d3.schemeCategory20c);
+    series = g.selectAll('.series').data(data.y01z).enter().append('g')
+              .attr('fill', (d, i) => color(i))
+              .attr('id', d => d.index);
+      rect = series.selectAll('rect').data(d => d).enter().append('rect')
+              .attr('x', (d, i) => x(i))
+              .attr('y', this.state.innerHeight)
+              .attr('width', x.bandwidth())
+              .attr('height', 0);
 
     rect.transition().delay((d, i) => i * 10)
-    .attr('y', d => y(d[1]))
-    .attr('height', d => y(d[0]) - y(d[1]));
+      .attr('y', d => y(d[1]))
+      .attr('height', d => y(d[0]) - y(d[1]));
 
     g.append('g').attr('class', 'axis axis--x')
-    .attr('transform', 'translate(0,' + this.state.innerHeight + ')')
-    .call(d3.axisBottom(x).tickSize(1).tickPadding(6));
+      .attr('transform', 'translate(0,' + this.state.innerHeight + ')')
+    .call(d3.axisBottom(x).tickSize(1).tickPadding(6).tickFormat(d => data.xz[d]));
 
     d3.selectAll('input').on('change', changed);
 
-    var timeout = d3.timeout(() => {
+    timeout = d3.timeout(() => {
       d3.select('input[value="grouped"]').property('checked', true).dispatch('change');
     }, 2000);
 
@@ -91,12 +130,12 @@ class StackedGroupedBar extends React.Component {
 
       rect.transition()
           .duration(500)
-          .delay(function(d, i) { return i * 10; })
-          .attr('x', function(d, i) { return x(i) + x.bandwidth() / data.n * this.parentNode.__data__.key; })
+          .delay((d, i) => i * 10)
+          .attr('x', (d, i, g) => x(i) + x.bandwidth() / data.n * g[0].parentNode.__data__.index)
           .attr('width', x.bandwidth() / data.n)
         .transition()
-          .attr('y', function(d) { return y(d[1] - d[0]); })
-          .attr('height', function(d) { return y(0) - y(d[1] - d[0]); });
+          .attr('y', d => y(d[1] - d[0]))
+          .attr('height', d => y(0) - y(d[1] - d[0]));
     }
 
     function transitionStacked() {
@@ -104,17 +143,17 @@ class StackedGroupedBar extends React.Component {
 
       rect.transition()
           .duration(500)
-          .delay(function(d, i) { return i * 10; })
-          .attr('y', function(d) { return y(d[1]); })
-          .attr('height', function(d) { return y(d[0]) - y(d[1]); })
+          .delay((d, i) => i * 10)
+          .attr('y', d => y(d[1]))
+          .attr('height', d => y(d[0]) - y(d[1]))
         .transition()
-          .attr('x', function(d, i) { return x(i); })
+          .attr('x', (d, i) => x(i))
           .attr('width', x.bandwidth());
     }
 
   }
 
-  bumps(m) {
+  bumps(m) { //only used for sampleData
     var values = [], i, j, w, x, y, z;
 
     for (i = 0; i < m; i++) {
