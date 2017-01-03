8ed57205e1976e2633b0b34fa4a13e5b1a6acb1b
Rob0h <robert.is.oh@gmail.com>
2017-01-02 22:02:08 -0800

    Implement material ui formatting for graph and properties

--- src/client/app/documentation/VoronoiDoc.jsx
+++ src/client/app/documentation/VoronoiDoc.jsx
@@ -1,16 +1,92 @@
 import React, {Component} from 'react';
 import Voronoi from '../voronoi/voronoi.jsx';
 
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import {Table, TableBody, TableFooter, TableHeader, TableHeaderColumn, TableRow, TableRowColumn}
+  from 'material-ui/Table';
+import Divider from 'material-ui/Divider';
+import FlatButton from 'material-ui/FlatButton';
+import Toggle from 'material-ui/Toggle';
+
+const propTable = [
+  {
+    name: 'Lorem',
+    type: 'Ipsum',
+    default: 'Dolor',
+    description: 'Sit',
+  },
+  {
+    name: 'consectetur',
+    type: 'adipiscing',
+    default: 'elit',
+    description: 'donec',
+  },
+]
+
 class VoronoiDoc extends Component {
   constructor() {
     super();
+    this.state = {
+      expanded: false
+    }
   }
+
+  handleExpandChange = (expanded) => {
+    this.setState({expanded: expanded});
+  };
 
   render() {
     return (
       <div className="container">
         <h2>Voronoi</h2>
-        <Voronoi />
+        <Divider />
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi. Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque. Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+        </p>
+        
+        <Card expanded={this.state.expanded} onExpandChange={this.handleExpandChange}>
+          <CardHeader
+            title="Example Voronoi"
+            subtitle="Subtitle"
+            actAsExpander={true}
+            showExpandableButton={true}
+          />
+          <CardText>
+            <Voronoi />
+          </CardText>
+          <CardMedia
+            expandable={true}
+            overlay={<CardTitle title="Overlay title" subtitle="Overlay subtitle" />}>
+          </CardMedia>
+          <CardTitle title="Codepen" subtitle="Card subtitle" expandable={true} />
+          <CardText expandable={true}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+            Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+            Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+          </CardText>
+        </Card>
+
+        <h3>Voronoi Properties</h3>
+        <Table>
+          <TableHeader displaySelectAll= {false} adjustForCheckbox = {false} >
+            <TableRow>
+              <TableHeaderColumn>Name</TableHeaderColumn>
+              <TableHeaderColumn>Type</TableHeaderColumn>
+              <TableHeaderColumn>Default</TableHeaderColumn>
+              <TableHeaderColumn>Description</TableHeaderColumn>
+            </TableRow>
+          </TableHeader>
+          <TableBody displayRowCheckbox = {false} >
+            {propTable.map((row, index) => (
+              <TableRow key={index}>
+                <TableRowColumn>{row.name}</TableRowColumn>
+                <TableRowColumn>{row.type}</TableRowColumn>
+                <TableRowColumn>{row.default}</TableRowColumn>
+                <TableRowColumn>{row.description}</TableRowColumn>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
       </div>
     )
   }
