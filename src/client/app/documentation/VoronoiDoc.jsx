a96de1824bb16133ab1954b4e4dff726276bb156
Rob0h <robert.is.oh@gmail.com>
2017-01-03 13:47:07 -0800

    Implement initial state of Voronoi codepen

--- src/client/app/documentation/VoronoiDoc.jsx
+++ src/client/app/documentation/VoronoiDoc.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import Voronoi from '../voronoi/voronoi.jsx';
+import Codepen from 'react-codepen';
 
 import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 import {Table, TableBody, TableFooter, TableHeader, TableHeaderColumn, TableRow, TableRowColumn}
@@ -55,10 +56,7 @@ class VoronoiDoc extends Component {
           </CardText>
           <CardTitle title="Codepen" subtitle="Card subtitle" expandable={true} />
           <CardText expandable={true}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
-            Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
-            Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+            <Codepen user="Rob0h" hash="YNzVRG" />
           </CardText>
         </Card>
 
