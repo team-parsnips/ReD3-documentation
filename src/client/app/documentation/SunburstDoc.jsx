01e85829c78add43fc26fc9351cc2b1465c3ee73
Rob0h <robert.is.oh@gmail.com>
2017-01-05 16:56:59 -0800

    Align styling of SunburstDoc

--- src/client/app/documentation/SunburstDoc.jsx
+++ src/client/app/documentation/SunburstDoc.jsx
@@ -1,5 +1,28 @@
 import React, {Component} from 'react';
 import Sunburst from '../sunburst/sunburst.jsx';
+import Codepen from './Codepen.jsx';
+
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import {Table, TableBody, TableFooter, TableHeader, TableHeaderColumn, TableRow, TableRowColumn}
+  from 'material-ui/Table';
+import Divider from 'material-ui/Divider';
+import FlatButton from 'material-ui/FlatButton';
+import Toggle from 'material-ui/Toggle';
+
+const propTable = [
+  {
+    name: 'Lorem',
+    type: 'Ipsum',
+    default: 'Dolor',
+    description: 'Sit',
+  },
+  {
+    name: 'consectetur',
+    type: 'adipiscing',
+    default: 'elit',
+    description: 'donec',
+  },
+]
 
 class SunburstDoc extends Component {
   constructor() {
@@ -10,7 +33,40 @@ class SunburstDoc extends Component {
     return (
       <div>
         <h2>Sunburst</h2>
-        <Sunburst />
+        <Divider />
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi. Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque. Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+        </p>
+
+        <Card>
+          <CardHeader
+            title="Example Scatter Plot"
+            subtitle="Subtitle"/>
+          <CardMedia>
+            <Sunburst />
+          </CardMedia>
+        </Card>
+        
+        <h3>Sunburst Properties</h3>
+        <Table>
+          <TableHeader displaySelectAll= {false} adjustForCheckbox = {false} >
+            <TableRow>
+              <TableHeaderColumn>Name</TableHeaderColumn>
+              <TableHeaderColumn>Type</TableHeaderColumn>
+              <TableHeaderColumn>Default</TableHeaderColumn>
+              <TableHeaderColumn>Description</TableHeaderColumn>
+            </TableRow>
+          </TableHeader>
+          <TableBody displayRowCheckbox = {false} >
+            {propTable.map((row, index) => (
+              <TableRow key={index}>
+                <TableRowColumn>{row.name}</TableRowColumn>
+                <TableRowColumn>{row.type}</TableRowColumn>
+                <TableRowColumn>{row.default}</TableRowColumn>
+                <TableRowColumn>{row.description}</TableRowColumn>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
       </div>
     )
   }
