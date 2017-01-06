8bcdf41c8547226a3ae1f17601821e475fb388b2
Rob0h <robert.is.oh@gmail.com>
2017-01-05 16:55:24 -0800

    Align styling of ScatterPlotDoc

--- src/client/app/documentation/ScatterPlotDoc.jsx
+++ src/client/app/documentation/ScatterPlotDoc.jsx
@@ -1,5 +1,28 @@
 import React, {Component} from 'react';
 import ScatterPlot from '../scatterPlot/scatterPlot.jsx';
+import Codepen from './Codepen.jsx';
+
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import {Table, TableBody, TableFooter, TableHeader, TableHeaderColumn, TableRow, TableRowColumn}
+  from 'material-ui/Table';
+import Divider from 'material-ui/Divider';
+import FlatButton from 'material-ui/FlatButton';
+import Toggle from 'material-ui/Toggle';
+
+const propTable = [
+  {
+    name: 'Lorem',
+    type: 'Ipsum',
+    default: 'Dolor',
+    description: 'Sit',
+  },
+  {
+    name: 'consectetur',
+    type: 'adipiscing',
+    default: 'elit',
+    description: 'donec',
+  },
+]
 
 class ScatterPlotDoc extends Component {
   constructor() {
@@ -10,7 +33,42 @@ class ScatterPlotDoc extends Component {
     return (
       <div>
         <h2>Scatter Plot</h2>
-        <ScatterPlot />
+        <Divider />
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi. Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque. Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+        </p>
+
+        <Card>
+          <CardHeader
+            title="Example Scatter Plot"
+            subtitle="Subtitle"/>
+          <CardMedia>
+            <ScatterPlot />
+          </CardMedia>
+        </Card>
+
+      <h3>Scatter Plot Properties</h3>
+        <Table>
+          <TableHeader displaySelectAll= {false} adjustForCheckbox = {false} >
+            <TableRow>
+              <TableHeaderColumn>Name</TableHeaderColumn>
+              <TableHeaderColumn>Type</TableHeaderColumn>
+              <TableHeaderColumn>Default</TableHeaderColumn>
+              <TableHeaderColumn>Description</TableHeaderColumn>
+            </TableRow>
+          </TableHeader>
+          <TableBody displayRowCheckbox = {false} >
+            {propTable.map((row, index) => (
+              <TableRow key={index}>
+                <TableRowColumn>{row.name}</TableRowColumn>
+                <TableRowColumn>{row.type}</TableRowColumn>
+                <TableRowColumn>{row.default}</TableRowColumn>
+                <TableRowColumn>{row.description}</TableRowColumn>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+
+        
       </div>
     )
   }
